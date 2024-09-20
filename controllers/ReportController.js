be4ce8a32e7a729f6fe62f61c0f45814fb36d91f
Valenti <valenti94br@gmail.com>
2024-09-20 06:37:10 +0200

    CRUD hecho

--- controllers/ReportController.js
+++ controllers/ReportController.js
@@ -19,6 +19,34 @@ const ReportController = {
             res.status(500).send({ msg: 'Ha habido un problema al cargar las noticias', error })
         }
     },
-}
+    async getById(req, res) {
+        try {
+            const report = await Report.findById(req.params._id)
+            res.send(report)
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ msg: 'Ha habido un problema al cargar la noticia', error })
+        }
+    },
+    async delete(req, res) {
+        try {
+            const report = await Report.findByIdAndDelete(req.params._id);
+            res.send({ message: 'Noticia eliminada', report });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ msg: 'Ha habido un problema al eliminar la noticia', error })
+        }
+    },
+    async update(req, res) {
+        try {
+            const report = await Report.findByIdAndUpdate(req.params._id, req.body,
+                { new: true })
+            res.send({ message: "Noticia actualizada", report });
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ msg: 'Ha habido un problema al actualizar la noticia', error })
+        }
+    },
+};
 
 module.exports = ReportController;
--- routes/reports.js
+++ routes/reports.js
@@ -4,5 +4,8 @@ const router = express.Router()
 
 router.post("/createReport",ReportController.createReport)
 router.get("/getAllReports",ReportController.getAll)
+router.get("/getById/:_id",ReportController.getById)
+router.delete("/deleteReportById/:_id",ReportController.delete)
+router.put("/updateReportById/:_id",ReportController.update)
 
 module.exports = router
